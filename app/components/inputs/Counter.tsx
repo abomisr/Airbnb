244552a8768fd2fdb64fc99c6b355c0bcf8da5bf
duxpixels <duxwork0@gmail.com>
2023-06-14 18:29:30 +0300

    Listing -> Counter

---
+++ app/components/inputs/Counter.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useCallback } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+interface CounterProps {
+    onChange: (value:number)=>void;
+    value: number;
+    title: string;
+    subtitle: string;
+}
+const Counter:React.FC<CounterProps> = ({
+    onChange,
+    value,
+    title,
+    subtitle,
+}) => {
+
+    const onAdd = useCallback(() =>{
+        onChange(value + 1)
+    },[value,onChange])
+
+    const onReduce = useCallback(() =>{
+        if(value === 1) return;
+
+        onChange(value - 1)
+    },[value,onChange])
+    
+  return (
+    <div className="justify-between items-center flex">
+        <div className="flex flex-col">
+            <div className="font-medium">
+                {title}
+            </div>
+            <div className="font-light text-gray-600">
+                {subtitle}
+            </div>
+        </div>
+        <div className="flex items-center gap-4">
+            <button onClick={onReduce} className="w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition">
+                <AiOutlineMinus />
+            </button>
+            <div className="font-light text-xl text-neutral-600">
+                {value}
+            </div>
+            <button onClick={onAdd} className="w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition">
+                <AiOutlinePlus />
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Counter
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { categories } from '../navbar/Categories';
 import useRentModal from '@/app/hooks/useRentModal';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
+import Counter from '../inputs/Counter';
 
 enum STEPS {
     CATEGORY = 0,
@@ -23,59 +24,63 @@ const RentModal = () => {
     const rentModal = useRentModal()
     const [step, setStep] = useState(STEPS.CATEGORY);
 
-    const {register,handleSubmit,setValue,watch,formState:{errors}} = useForm<FieldValues>({
-        defaultValues:{
-            category:"",
-            location:null,
-            guestCount:1,
-            roomCount:1,
-            bathroomCount:1,
-            imageSrc:"",
-            price:1,
-            title:"",
-            description:""
+    const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm<FieldValues>({
+        defaultValues: {
+            category: "",
+            location: null,
+            guestCount: 1,
+            roomCount: 1,
+            bathroomCount: 1,
+            imageSrc: "",
+            price: 1,
+            title: "",
+            description: ""
         }
     })
 
     const category = watch("category");
     const location = watch("location");
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc');
 
-    const Map = useMemo(()=> dynamic(()=>import("../Map"), {ssr:false}) ,[location])
+    const Map = useMemo(() => dynamic(() => import("../Map"), { ssr: false }), [location])
 
-    const setCustomValue = (id:string,value:any)=>{
-        setValue(id,value,{
-            shouldValidate:true,
-            shouldDirty:true,
-            shouldTouch:true
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldValidate: true,
+            shouldDirty: true,
+            shouldTouch: true
         })
     }
 
-    const onBack = () =>{
-        setStep(value=> value-1)
+    const onBack = () => {
+        setStep(value => value - 1)
     }
-    const onNext = () =>{
-        setStep(value=> value+1)
+    const onNext = () => {
+        setStep(value => value + 1)
     }
 
 
-    const actionLabel = useMemo(()=>{
-        if(step === STEPS.PRICE) return "Submit"
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.PRICE) return "Submit"
         return "Next"
-    },[step])
+    }, [step])
 
-    const secondaryActionLabel = useMemo(()=>{
-        if(step === STEPS.CATEGORY) return undefined
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) return undefined
         return "Back"
-    },[step])
+    }, [step])
 
     let bodyContent = [
         (
             <div className="flex flex-col gap-8">
                 <Heading title="Which of these best describes your place?" subtitle='Pick a category' />
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-                    {categories.map((item)=>(
+                    {categories.map((item) => (
                         <div key={item.label} className='col-span-1'>
-                            <CategoryInput onClick={(category)=>setCustomValue("category",category)} selected={item.label === category} icon={item.icon} label={item.label} />
+                            <CategoryInput onClick={(category) => setCustomValue("category", category)} selected={item.label === category} icon={item.icon} label={item.label} />
                         </div>
                     ))}
                 </div>
@@ -84,29 +89,57 @@ const RentModal = () => {
         (
             <div className='flex flex-col gap-8'>
                 <Heading title='Where is your place located?' subtitle='Help guests find you!' />
-                <CountrySelect 
-                    onChange={(value)=>setCustomValue("location",value)}
+                <CountrySelect
+                    onChange={(value) => setCustomValue("location", value)}
                     value={location}
                 />
-                <Map 
+                <Map
                     center={location?.latlng}
+                />
+            </div>
+        ),
+        (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenitis do you have?"
+                />
+                <Counter
+                    onChange={(value) => setCustomValue('guestCount', value)}
+                    value={guestCount}
+                    title="Guests"
+                    subtitle="How many guests do you allow?"
+                />
+                <hr />
+                <Counter
+                    onChange={(value) => setCustomValue('roomCount', value)}
+                    value={roomCount}
+                    title="Rooms"
+                    subtitle="How many rooms do you have?"
+                />
+                <hr />
+                <Counter
+                    onChange={(value) => setCustomValue('bathroomCount', value)}
+                    value={bathroomCount}
+                    title="Bathrooms"
+                    subtitle="How many bathrooms do you have?"
                 />
             </div>
         )
     ]
-    
+
     return (
-    <Modal
-    title ="Airbnb your home!"
-    actionLabel={actionLabel}
-    onSubmit={onNext}
-    secondaryActionLabel={secondaryActionLabel}
-    secondaryAction={step === STEPS.CATEGORY ? undefined:onBack}
-    isOpen={rentModal.isOpen}
-    onClose={rentModal.onClose}
-    body={bodyContent[step]}
-    />
-  )
+        <Modal
+            title="Airbnb your home!"
+            actionLabel={actionLabel}
+            onSubmit={onNext}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            body={bodyContent[step]}
+        />
+    )
 }
 
 export default RentModal
