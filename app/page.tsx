42298c7682afcbc79f9db438dba766ed44342a61
duxpixels <duxwork0@gmail.com>
2023-06-18 00:52:50 +0300

    Listing card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,54 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import EmptyState from "./components/EmptyState";
+import getListings, { IListingsParams } from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
 
 
-export default function Home() {
+interface HomeProps {
+  searchParams: IListingsParams
+};
+
+export default async function Home({ searchParams }: HomeProps) {
+
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <main className="text-rose-500 text-2xl">
-      Hello world!
-    </main>
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,18 @@
-import { User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
+
+export type SafeListing = Omit<Listing, "joinedAt"> & {
+  joinedAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation, 
+  "joinedAt" | "startDate" | "endDate" | "listing"
+> & {
+  joinedAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
 
 export type SafeUser = Omit<
   User,
