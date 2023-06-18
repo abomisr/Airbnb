dc9437c57e86f9beec4617c5897378e86221df41
duxpixels <duxwork0@gmail.com>
2023-06-18 11:30:19 +0300

    Listing page and listing reservation

---
+++ app/actions/getListingById.ts
@@ -0,0 +1,40 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export default async function getListingById(
+  params: IParams
+) {
+  try {
+    const { listingId } = params;
+
+    const listing = await prisma.listing.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true
+      }
+    });
+
+    if (!listing) {
+      return null;
+    }
+
+    return {
+      ...listing,
+      joinedAt: listing.joinedAt.toString(),
+      user: {
+        ...listing.user,
+        joinedAt: listing.user.joinedAt.toString(),
+        updatedAt: listing.user.updatedAt.toString(),
+        emailVerified: 
+          listing.user.emailVerified?.toString() || null,
+      }
+    };
+  } catch (error: any) {
+    return null
+  }
+}
